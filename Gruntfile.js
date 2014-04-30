64ca7a8f8b70315042254e65a15725a7c98cd6dd
Ibe Vanmeenen <ibevanmeenen@hotmail.com>
2014-05-01 00:39:14 +0200

    setup

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        watch: {
+            sass: {
+                files: ['./scss/**/*.scss'],
+                tasks: ['sass']
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    './css/Quotify.css': './scss/Quotify.scss'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('default', ['sass', 'watch']);
+};
